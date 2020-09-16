33bc4438bedb19b24418075592953a278a16a2a6
Arnaldo Souza <arnaldoeloi2@gmail.com>
2020-09-16 20:53:11 -0300

    Init

---
+++ Assets/Scripts/Flipper.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flipper : MonoBehaviour
+{
+    public float restPos    = 0f;
+    public float posOnPress = 45f;
+    public float hitStrenght = 100f;
+    public float flipperDamping = 160f;
+
+    public string inputType  = "left";
+
+    private HingeJoint hinge;
+
+    private JointSpring spring;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hinge = GetComponent<HingeJoint>();
+        hinge.useSpring = true;
+        hinge.useLimits = true;
+        //hinge.motor.force = this.hitStrenght;
+        spring = new JointSpring();
+        spring.spring = hitStrenght;
+        spring.damper = flipperDamping;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(inputType))
+        {
+            spring.targetPosition = posOnPress;
+        }
+        else
+        {
+            spring.targetPosition = restPos;
+        }
+
+        hinge.spring = spring;
+        
+
+        //if (Input.GetAxis("Horizontal") < -0.1f)
+        //{
+        //    hinge.useMotor = false;
+        //    Debug.Log("Left");
+        //}
+    }
+}
---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    public float maxForce = 10.0f;
+    private float forceTarget = 0f;
+    private bool releasing = false;
+    private Rigidbody rb;
+
+    private Vector3 originalPosition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.originalPosition = transform.position;
+        this.rb = GetComponent<Rigidbody>();
+        // this.rb.isKinematic = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButton("Vertical") && !releasing){
+            this.forceTarget = Mathf.Clamp(this.forceTarget + 10f, 0f, this.maxForce);
+            transform.Translate(Vector3.forward * -0.02f) ;
+            Debug.Log(this.forceTarget);
+        }
+
+        else if(Input.GetButtonUp("Vertical") && !releasing){
+            // rb.isKinematic = true;
+            releasing = true;
+            // rb.AddForce(transform.forward * currentForce * 1000, ForceMode.Impulse);
+        }
+
+        else if(releasing){
+            Debug.Log("RELEASE! "+forceTarget);
+            
+            rb.AddForce(transform.forward*forceTarget, ForceMode.VelocityChange);
+            
+            if(transform.position.z >= originalPosition.z ){
+                rb.velocity = Vector3.zero;
+                transform.position = originalPosition;
+                releasing = false;
+            }
+
+            this.forceTarget=0f;
+        }
+    }
+}
